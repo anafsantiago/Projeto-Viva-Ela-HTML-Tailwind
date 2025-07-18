df8925f947a914f84a7b8bf6878a2712166b1721
anafsantiago <fla.santiago09@gmail.com>
2025-07-18 00:16:23 -0300

    parte recursividade do menu mobile

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,41 @@
+import outsideClick from "./click-outside.js";
+
+export default class MenuMobile {
+  constructor(menuButton, menuList, events) {
+    this.menuButton = document.querySelector(menuButton);
+    this.menuList = document.querySelector(menuList);
+    this.activeClass = "active";
+
+    //Define touchstart e click como argumento padrão de events, caso o usuário não defina
+    if (events === undefined) {
+      this.events = ["click", "touchstart"];
+    } else {
+      this.events = events;
+    }
+
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  openMenu(event) {
+    event.preventDefault();
+    this.menuButton.classList.add(this.activeClass);
+    this.menuList.classList.add(this.activeClass);
+    outsideClick(this.menuList, this.events, () => {
+      this.menuButton.classList.remove(this.activeClass);
+      this.menuList.classList.remove(this.activeClass);
+    });
+  }
+
+  addMenuMobileEvents() {
+    this.events.forEach((userEvent) => {
+      this.menuButton.addEventListener(userEvent, this.openMenu);
+    });
+  }
+
+  init() {
+    if (this.menuButton && this.menuList) {
+      this.addMenuMobileEvents();
+    }
+    return this;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 import ScrollAnima from "./modules/scroll-anima.js";
 import MenuDropdown from "./modules/menu-dropdown.js";
+import MenuMobile from "./modules/menu-mobile.js";
 
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
 
 const menuDropdown = new MenuDropdown("[data-dropdown]");
 menuDropdown.init();
+
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
