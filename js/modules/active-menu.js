201a608bca61eddfb454f18441aae83227fe8ea8
Ana Flávia <fla.santiago09@gmail.com>
2025-08-05 13:11:09 -0300

    testes no console de ActiveMenu

--- js/modules/active-menu.js
+++ js/modules/active-menu.js
@@ -25,7 +25,10 @@ export default class ActiveMenu {
     this.links.forEach((link) => {
       link.classList.remove(this.activeClass);
       const href = link.getAttribute("href").split("./");
-
+      console.log("URL: " + this.url[1]);
+      console.log("HREF completo " + href);
+      console.log(href[1] === this.url[1]);
+      console.log("HREF " + href[1]);
       if (href[1] === this.url[1]) {
         link.classList.add(this.activeClass);
       }
